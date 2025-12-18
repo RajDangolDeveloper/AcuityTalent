f38879a57850c0c368b2f54e112bf3ab7535aec6
RajDangolDeveloper <np01cp4s240063@islingtoncollege.edu.np>
2025-12-18 23:17:30 +0545

    Delete backend/dist directory

--- backend/dist/src/controllers/auth/auth.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { LoginService } from '../users/login.service';
-import { LoginDto } from './dto/login.dto';
-export declare class LoginController {
-    private readonly loginService;
-    constructor(loginService: LoginService);
-    getHello(): string;
-    loginUser(loginData: LoginDto): Promise<any>;
-}
